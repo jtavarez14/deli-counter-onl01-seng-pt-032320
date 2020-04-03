e6103a6d7ad72bd27c524cc73cf1a67f6349dae5
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-03 17:44:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli) 
+  people = 0
+  if people == 0 
+    puts "The line is currently empty."
+end
+end
+
+line(katz_deli)
+
+def take_a_number(katz_deli, name)
+  katz_deli << "name"
+  people = 1 
+    if people > 0  
+    katz_deli.each_with_index do |katz_deli, index|
+      puts "The line is currently: #{index + 1}. #{katz_deli}"
+  people += 1
+    end
+  end
+end
+
+take_a_number(katz_deli, "name")
